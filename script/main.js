aea580909dd42bbe9157f531a898086f0b34248a
Your NameIgor-Kay <kaygorodtsevie@gmail.com>
2024-01-06 18:17:05 +0700

    mb final

--- script/main.js
+++ script/main.js
@@ -1,8 +1,5 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.133.1';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/loaders/GLTFLoader.js';
-import { EffectComposer } from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/postprocessing/RenderPass.js';
-import { UnrealBloomPass } from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/postprocessing/UnrealBloomPass.js';
 
 var scene = new THREE.Scene();
 var renderer = new THREE.WebGLRenderer({
@@ -10,20 +7,14 @@ var renderer = new THREE.WebGLRenderer({
   alpha: true
 });
 renderer.outputEncoding = THREE.sRGBEncoding;
-renderer.setPixelRatio(window.devicePixelRatio || 2);
+renderer.setPixelRatio(window.devicePixelRatio || 1);
 
 var camera = new THREE.PerspectiveCamera(10, 1, 1, 1000);
-camera.position.set(0, 0, 4);
+camera.position.set(0, 0, 7.5);
 
 var canvas = renderer.domElement;
 document.querySelector('#character').appendChild(canvas);
 
-var composer = new EffectComposer(renderer);
-var renderPass = new RenderPass(scene, camera);
-composer.addPass(renderPass);
-
-var bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 2, 0.4, .85);
-composer.addPass(bloomPass);
 
 const modelPath = 'assets/scene (2).gltf';
 
@@ -35,7 +26,7 @@ let mixer;
 loader.load(modelPath, function (gltf) {
 
   model = gltf.scene;
-  const scale = 0.15;
+  const scale = 0.4;
   model.scale.set(scale, scale, scale);
   base.add(model);
   mixer = new THREE.AnimationMixer(model);
@@ -60,13 +51,12 @@ renderer.setAnimationLoop(() => {
   if (resize(renderer)) {
     camera.aspect = canvas.clientWidth / canvas.clientHeight;
     camera.updateProjectionMatrix();
-    composer.setSize(canvas.clientWidth, canvas.clientHeight);
   }
   if (mixer) {
     mixer.update(delta);
   }
 
-  composer.render(delta);
+  renderer.render(scene, camera);
 });
 
 function resize(renderer) {
